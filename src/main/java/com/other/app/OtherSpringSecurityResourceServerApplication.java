f27e813e8a40705e5465e6fcbe34245408cb5179
U-235-92 <a745296@yandex.ru>
2024-08-06 15:33:54 +0400

    Empty commit

---
+++ src/main/java/com/other/app/OtherSpringSecurityResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.other.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtherSpringSecurityResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtherSpringSecurityResourceServerApplication.class, args);
+	}
+
+}
